73a25559d8f3047181dbd77b9ed3eb1e17edd6f6
Daniel Arroyo <daniel.arroyo@wizeline.com>
2023-09-27 23:37:36 -0600

    folder for scripts

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
-./start_minikube.sh
+./start_scripts/start_minikube.sh
 
 helm repo update
 
-(./start_dynamodb.sh
-./load_dynamo_tables.sh) &
+(./start_scripts/start_dynamodb.sh
+./start_scripts/load_dynamo_tables.sh) &
 
-#(./start_postgres.sh
-#./connect_postgres_local.sh) &
+#(./start_scripts/start_postgres.sh
+#./start_scripts/connect_postgres_local.sh) &
 
-./start_kafka-cluster.sh &
+./start_scripts/start_kafka-cluster.sh &
 
-./start_quote-poll.sh &
-./start_api.sh &
-#./start_front.sh &
-./start_stock-prices-collector.sh &
-./start_stock-prices-sink.sh &
+./start_scripts/start_quote-poll.sh &
+./start_scripts/start_api.sh &
+#./start_scripts/start_front.sh &
+./start_scripts/start_stock-prices-collector.sh &
+./start_scripts/start_stock-prices-sink.sh &
 
 #kafka-console-consumer.bat --bootstrap-server 127.0.0.1:31090 --topic market.quotes.price
 #kafka-console-consumer.bat --bootstrap-server 127.0.0.1:31090 --topic market.stock.price.last
